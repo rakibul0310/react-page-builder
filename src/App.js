c30d37c9a427333969e1fe2854eee284d77ec06b
rakibul2180@gmail.com <rakibul2180@gmail.com>
2023-07-08 15:38:44 +0600

    home page added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Editor from "./pages/Editor";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
